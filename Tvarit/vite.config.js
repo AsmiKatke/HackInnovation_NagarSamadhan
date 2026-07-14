601568c4a773309fa2136b1f7b081fa085adc24e
Scarlett1625 <112947574+Scarlett1625@users.noreply.github.com>
2026-07-14 20:47:35 +0530

    Updated header to NagarSamadhan

--- Tvarit/vite.config.js
+++ Tvarit/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/HackInnovation_Tvarit/',
+  base: '/NagarSamadhan/',
   plugins: [react()], // (or whatever plugins are here)
 })
